754486bcadfff24218563a86ef4d812c0c968334
sinta1919 <92878485+sinta1919@users.noreply.github.com>
2022-03-01 19:20:58 +0700

    Add files via upload

---
+++ tugas pv/tugas pv/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 01/03/2022
+ * Time: 17:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace tugas_pv
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(131, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(110, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Daftar Nama Makeup";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(51, 83);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Nama Barang";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(157, 83);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(117, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(64, 141);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(199, 82);
+			this.listBox1.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(283, 120);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "tambah";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(370, 278);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "tugas pv";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ tugas pv/tugas pv/MainForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 01/03/2022
+ * Time: 17:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tugas_pv
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e) 
+		{
+	
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(textBox1.Text);
+			textBox1.Clear();
+			textBox1.Focus();
+		}
+	}
+}
